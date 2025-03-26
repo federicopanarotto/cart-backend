c76b9a0f8da7a270bc57c9d50eae0d4aba87eabd
Federico Panarotto <104077886+federicopanarotto@users.noreply.github.com>
2025-03-26 18:41:54 +0100

    sus

--- src/api/product/product.dto.ts
+++ src/api/product/product.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer";
-import { IsNumber, IsOptional, IsString, Min } from "class-validator";
+import { IsNumber, IsOptional, IsString, Min, registerDecorator, Validate, ValidationArguments, ValidationOptions } from "class-validator";
 
 export class QueryProductsDTO {
     @IsString()
@@ -14,7 +14,28 @@ export class QueryProductsDTO {
     
     @IsNumber()
     @IsOptional()
-    @Min(0)
+    @IsGreaterThanMin('')
     @Type(() => Number)
     maxPrice?: number;
+}
+
+export function IsGreaterThanMin(property: string, validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            name: 'IsGreaterThanMin',
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            validator: {
+                validate(value: any, args: ValidationArguments) {
+                    console.log(args.object)
+                    const currentMinPrice = Object.values(args.object)[0];
+                    return value >= currentMinPrice;
+                },
+                defaultMessage(args: ValidationArguments) {
+                    return "MaxPrice value must be higher than minPrice value";
+                }
+            }
+        })
+    }
 }
